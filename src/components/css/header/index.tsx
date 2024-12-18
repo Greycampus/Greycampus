562f43edbf91cd059a56fb55ff583d93fcae4e5e
Sharanmax <sharanraomax@gmail.com>
2024-12-19 02:32:28 +0530

    fix: header

--- src/components/css/header/index.tsx
+++ src/components/css/header/index.tsx
@@ -34,56 +34,58 @@ const Header = () => {
 
   return (
     <>
-      {/* Top Header */}
-      <AppBar position="static" sx={{ backgroundColor: "black" }}>
-        <Toolbar>
-          {/* Logo */}
-          <Box sx={{ flexGrow: 1 }}>
-            <Link href="/" passHref>
-              <Image src={LOGO_URl} alt="GreyCampus Logo" width={150} height={40} priority />
-            </Link>
-          </Box>
-
-          {/* Desktop Navigation */}
-          <Box sx={{ display: { xs: "none", sm: "flex" }, gap: '24px' }}>
-            {!drawerOpen && navItems.map((item) => (
-              <Link key={item.name} href={item.link} passHref>
-                <Typography
-                  variant="body1"
-                  sx={{ marginRight: 3, color: "white", textDecoration: "none", cursor: "pointer" }}
-                >
-                  {item.name}
-                </Typography>
+      <Box sx={{ bgcolor: '#000', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        {/* Top Header */}
+        <AppBar position="static" sx={{ backgroundColor: "black", maxWidth: '1280px' }}>
+          <Toolbar>
+            {/* Logo */}
+            <Box sx={{ flexGrow: 1 }}>
+              <Link href="/" passHref>
+                <Image src={LOGO_URl} alt="GreyCampus Logo" width={150} height={40} priority />
               </Link>
-            ))}
+            </Box>
+
+            {/* Desktop Navigation */}
+            <Box sx={{ display: { xs: "none", sm: "flex" }, gap: '24px' }}>
+              {!drawerOpen && navItems.map((item) => (
+                <Link key={item.name} href={item.link} passHref>
+                  <Typography
+                    variant="body1"
+                    sx={{ marginRight: 3, color: "white", textDecoration: "none", cursor: "pointer" }}
+                  >
+                    {item.name}
+                  </Typography>
+                </Link>
+              ))}
+            </Box>
+
+            {/* Mobile Menu Icon */}
+            <IconButton
+              edge="end"
+              color="inherit"
+              aria-label="menu"
+              sx={{ display: { sm: "none" }, bgcolor: '#0C868D', height: '40px', width: '40px', borderRadius: '2px' }}
+              onClick={toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+
+        {/* Side Drawer for Mobile */}
+        <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)} PaperProps={{
+          sx: { backgroundColor: "#000", color: "#fff" }, // Black background, white text
+        }}>
+          <Box sx={{ width: 250, }} role="presentation" onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)}>
+            <List>
+              {navItems.map((item) => (
+                <MenuLink key={item.name} href={item.link} text={item.name} />
+              ))}
+            </List>
           </Box>
+        </Drawer>
 
-          {/* Mobile Menu Icon */}
-          <IconButton
-            edge="end"
-            color="inherit"
-            aria-label="menu"
-            sx={{ display: { sm: "none" }, bgcolor: '#0C868D', height: '40px', width: '40px', borderRadius: '2px' }}
-            onClick={toggleDrawer(true)}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-
-      {/* Side Drawer for Mobile */}
-      <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)} PaperProps={{
-        sx: { backgroundColor: "#000", color: "#fff" }, // Black background, white text
-      }}>
-        <Box sx={{ width: 250, }} role="presentation" onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)}>
-          <List>
-            {navItems.map((item) => (
-              <MenuLink key={item.name} href={item.link} text={item.name} />
-            ))}
-          </List>
-        </Box>
-      </Drawer>
-
+      </Box>
       <Divider sx={{ bgcolor: '#0C868D'}}/>
     </>
   );
