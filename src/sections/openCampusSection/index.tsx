01eee18fd9714cb0944c77a5097a351afdf38a71
jatinbky64 <jatin@odinschool.com>
2025-01-27 16:06:36 +0530

    resolve conflict

--- src/sections/openCampusSection/index.tsx
+++ src/sections/openCampusSection/index.tsx
@@ -107,7 +107,7 @@ const OpenCampusSection: React.FC = ({posts}: any) => {
                 </Box>
             
             </Box>
-            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', bgcolor: '#000', py: '48px', px: '16px', gap:"24px" }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', bgcolor: '#000', py: '48px', px: '16px', gap:3 }}>
                 {posts.map((item: any) =>
                     <Box key={item.id} sx={{ maxWidth: '900px', width: '100%' }}>
                         <CustomCard {...item} onReadMorePath={getStringPath(item.documentId)} isOpenCampusBlog={true}/>
