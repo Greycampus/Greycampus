2d071b9da36eebead9be119c9fd4da2a0879213c
jatinbky64 <jatin@odinschool.com>
2025-02-14 16:08:20 +0530

    skelton loader added

--- src/components/hubspot-form/index.jsx
+++ src/components/hubspot-form/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import { useRouter } from "next/router";
+import { Skeleton, Box, Grid, TextField, Button } from "@mui/material";
 
 const HubspotForm = ({ formId }) => {
   const router = useRouter();
@@ -12,17 +13,16 @@ const HubspotForm = ({ formId }) => {
       script.src = "//js.hsforms.net/forms/embed/v2.js";
       script.async = true;
       script.defer = true;
-  
+
       script.onload = () => {
         setIsLoaded(true);
       };
-  
+
       setTimeout(() => document.body.appendChild(script), 5000); // Delay loading by 5 seconds
     } else {
       setIsLoaded(true);
     }
   }, []);
-  
 
   useEffect(() => {
     if (isLoaded && window.hbspt) {
@@ -38,7 +38,48 @@ const HubspotForm = ({ formId }) => {
     }
   }, [isLoaded, formId, router]);
 
-  return <div id={formId} ref={formRef}></div>;
+  return (
+    <Box
+      ref={formRef}
+     
+    >
+      {!isLoaded ? (
+        // Skeleton Loader UI
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Skeleton variant="rectangular" width="100%" height={50} />
+          </Grid>
+          <Grid item xs={6}>
+            <Skeleton variant="rectangular" width="100%" height={50} />
+          </Grid>
+          <Grid item xs={12}>
+            <Skeleton variant="rectangular" width="100%" height={50} />
+          </Grid>
+          <Grid item xs={4}>
+            <Skeleton variant="rectangular" width="100%" height={50} />
+          </Grid>
+          <Grid item xs={8}>
+            <Skeleton variant="rectangular" width="100%" height={50} />
+          </Grid>
+          <Grid item xs={6}>
+            <Skeleton variant="rectangular" width="100%" height={50} />
+          </Grid>
+          <Grid item xs={6}>
+            <Skeleton variant="rectangular" width="100%" height={50} />
+          </Grid>
+          <Grid item xs={12}>
+            <Skeleton variant="rectangular" width="100%" height={100} />
+          </Grid>
+          <Grid item xs={12}>
+            <Skeleton variant="rectangular" width={150} height={50} />
+          </Grid>
+        </Grid>
+      ) : (
+        // HubSpot Form
+        <div id={formId} style={{ width: "100%" }}></div>
+      )}
+    </Box>
+  );
 };
 
 export default HubspotForm;
